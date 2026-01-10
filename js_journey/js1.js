4709b19b9d9b401fb9c3bfa3b67845223dabef2c
Nav_6391 <singhnaveen9795@gmail.com>
2026-01-10 09:26:52 +0000

    read this

---
+++ js_journey/js1.js
@@ -0,0 +1,30 @@
+// ";" it is use for a good practice use it but not necceseary to use it "js is to forgiving languge so no problem"
+
+console.log("hello") // use for print somthing on screen 
+
+ const id = 288393 // its a keyword after use of this you naver change any thing in this it means a constant
+ let email = "naveen8989@google.com"  // use it every where , you want 
+ var password = "23333" // now we did'nt use it if you want to know more about then search it 
+  account_city = "shimla" // it is a another way its taken as a wronng practice so did'nt use it 
+   
+// id = 2 //not  allowed 
+console.log(id);
+
+; // its like a table use it your self 
+
+// prefer not to use var 
+// becuz of issue in block scope and function scope 
+
+let account_state; // the vale is undifined in js if you did'nt assign any thing to variable it taken as a undifined value 
+console.table(id,email,password, account_city);
+
+// ajj ke liye itna hi meet you in next code 
+
+
+
+
+
+
+
+
+  
