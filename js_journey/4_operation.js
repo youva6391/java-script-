149b20dcfda8fb8d0648a43ea566c5ad5f07abc4
Nav_6391 <singhnaveen9795@gmail.com>
2026-01-11 10:29:33 +0000

    about_operation_in_js

---
+++ js_journey/4_operation.js
@@ -0,0 +1,28 @@
+// **********************************operations****************************************
+console.log(2+2);
+console.log(2*2);
+console.log(2-2);
+console.log(2**2); // js taking this as a power of like 2**2=4 3**3=9
+console.log(2/2);
+console.log(2%2); // its a modulus giving the  remainder like "0"
+
+//******************Concatenation ***************************************** 
+
+let a = "hello"
+let b = "world"
+
+console.log(a+b); // hello world use only on string:
+
+console.log("1" + 2);
+console.log("1"+"2");
+console.log(1+"2"); //  giving -- 12 
+console.log("1" + 2 + 2); // giving -- 122 but
+console.log(1 + 2 +"1"); // giving -- 31
+
+
+// learn about prefix and postfix for /MDN/ "++x" "x++" 
+
+//this is the end of this session 
+
+// ajj ke liye itna hi other things are in next code 
+
