8b942929e547dec0e3952535edf61163f9ad0884
Nav_6391 <singhnaveen9795@gmail.com>
2026-01-12 13:34:04 +0000

    some_math_in_js

---
+++ js_journey/10_maths_in_.js
@@ -0,0 +1,21 @@
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+ console.log(Math); 
+ console.log(Math.abs(-4));// it used for absolute vale of 4 means change of negative no.
+ console.log(Math.round(4.6));// use of round off 
+ console.log(Math.ceil(4.2)); // gives max no. output is : 5
+ console.log(Math.floor(4.9)); // gives min value : output is : 4
+ console.log(Math.min(4, 3, 6, 8)); 
+ console.log(Math.max(4, 3, 6, 8)); 
+
+console.log(Math.random()); //The JavaScript Math.random() method generates a pseudo-random floating-point number between 0 (inclusive) and 1 (exclusive)
+
+
+// ***************************** Search about these codes meaning yourself************************************
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
