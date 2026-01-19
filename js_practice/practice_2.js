e4c4e4d57a4473bce12c3cbcab90889ae006ffcf
Nav_6391 <singhnaveen9795@gmail.com>
2026-01-19 09:31:48 +0000

    reverse_table and normal table

---
+++ js_practice/practice_2.js
@@ -0,0 +1,25 @@
+// ########### Multiplication Table of 2 #########
+
+let table = 2
+for (let no2 = 0; no2 <= 10; no2++) {
+    console.log(no2 + " " + "*"+ " " + table +" "+ "=" + no2*table);
+    
+    
+}
+
+// ########### Multiplication Table of any number #########
+
+let table1 = 2
+for (let no3 = 0; no3 <= 10; no3++) {
+    console.log(no3 + " " + "*"+ " " + table1 +" "+ "=" + no3*table1);
+    
+    
+}
+// ########### Multiplication Table of any number in reverse order #########
+
+let table2 = 3
+for (let table3 = 10; table3 >= 0; table3--) {
+    console.log(table3 + " " + "*"+ " " + table2 +" "+ "=" + table3*table2);
+    
+    
+}
