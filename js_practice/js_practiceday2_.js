2942f5ed398e5ee5ab044c3b8a91db52f0bb0cc3
Nav_6391 <singhnaveen9795@gmail.com>
2026-01-20 10:00:41 +0000

    another prectice day2

---
+++ js_practice/js_practiceday2_.js
@@ -0,0 +1,11 @@
+let myarray = [ 10,30,40,50]
+let sum = 0 
+for (let i = 0; i < myarray.length; i++) {
+    sum = sum + myarray[i]
+    
+}
+let average = sum/myarray.length;
+console.log("sum", sum);
+console.log("average", average);
+
+
