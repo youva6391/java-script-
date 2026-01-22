efe8001f337ffa90d3678f34c54d5e1951050b3d
Nav_6391 <singhnaveen9795@gmail.com>
2026-01-22 06:35:33 +0000

    correction of last one  of fizz buzz  task

--- js_practice/day4practice_.js
+++ js_practice/day4practice_.js
@@ -1,28 +1,27 @@
 let input = Number(prompt("enter a no. :-"))
 
 function fizzbuzz(input) {
-    for (let i = 1; i <= input; i++) {
-        if (isNaN(input) || input <= 0) {
-            console.log("enter a valid no.");
-        break
 
-        } else if (i % 3 == 0 && i % 5 == 0) {
-            console.log(i+ "-"+ "fizzbuzz");
-
-        } else if (i % 3 == 0) {
-            console.log(i+ "-"+ "fizz");
-
-        } else if (i % 5 == 0) {
-            console.log(i+ "-"+ "buzz");
-
-        } else {
-            console.log(i + " - " + i);
-
+    if (isNaN(input) || input <= 0) {
+       console.log("enter a valid no.");
+     return;
 
         }
+        for (let i = 1; i <= input; i++) {
+
+        if (i % 3 == 0 && i % 5 == 0) {
+         console.log(i + "-" + "fizzbuzz");
+
+         } else if (i % 3 == 0) {
+            console.log(i + "-" + "fizz");
+
+         } else if (i % 5 == 0) {
+                    console.log(i + "-" + "buzz");
+
+         } else {
+             console.log(i + " - " + i);
 
 
-    }
-
-}
-fizzbuzz(20)
+         }
+         }
+        }
