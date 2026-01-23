42bc6849e8e3c365609f762d27872ea16d1de1db
Nav_6391 <singhnaveen9795@gmail.com>
2026-01-23 13:22:51 +0000

    another related to matrix

---
+++ js_practice/day5_task3.js
@@ -0,0 +1,12 @@
+let n = Number(prompt("enter a no."))
+
+for (let i = 0; i <= n ; i++) {
+   
+    for (let j = 2; j <= n; j++) {
+        row = row + " " + j
+         process.stdout.write(i)
+
+        
+    }
+console.log()    
+}
